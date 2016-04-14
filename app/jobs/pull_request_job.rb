5984cdbea2adef2b87e1da893a1504f9946f852e
Yannick Schutz <yannick.schutz@gmail.com>
2016-04-14 19:32:23 +0200

    Log message

--- app/jobs/pull_request_job.rb
+++ app/jobs/pull_request_job.rb
@@ -4,7 +4,10 @@ class PullRequestJob < ActiveJob::Base
   def perform(secret, pull_request_body)
     payload = JSON.parse(pull_request_body)
     return unless %w{opened synchronize}.include?(payload["action"])
-    return unless repositories.include?(payload["repository"]["full_name"])
+    unless repositories.include?(payload["repository"]["full_name"])
+      Rails.logger.info"#{payload["repository"]["full_name"]} is not in #{repositories}"
+      return
+    end
     pr = PullRequest.find_or_create_from_webhook(payload)
     pr.sha = payload["pull_request"]["head"]["sha"]
     pr.save
