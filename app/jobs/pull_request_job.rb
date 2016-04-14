2ba5f46efe3e54ff18c70f0188ba778532e0b7ce
Yannick Schutz <yannick.schutz@gmail.com>
2016-04-14 19:27:55 +0200

    Limit to repositories

--- app/jobs/pull_request_job.rb
+++ app/jobs/pull_request_job.rb
@@ -4,6 +4,7 @@ class PullRequestJob < ActiveJob::Base
   def perform(secret, pull_request_body)
     payload = JSON.parse(pull_request_body)
     return unless %w{opened synchronize}.include?(payload["action"])
+    return unless repositories.include?(payload["repository"]["full_name"])
     pr = PullRequest.find_or_create_from_webhook(payload)
     pr.sha = payload["pull_request"]["head"]["sha"]
     pr.save
@@ -15,5 +16,9 @@ class PullRequestJob < ActiveJob::Base
         "No vulnerabilities found"
       end
     pr.create_status(state, description)
+  end
+
+  def repositories
+    JSON.parse(File.read(File.join(Rails.root, "config", "repositories.json")))
   end
 end
