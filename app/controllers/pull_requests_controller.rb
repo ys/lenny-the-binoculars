ccb10dd9df460f163308167d052ce40cf84abe42
Yannick Schutz <yannick.schutz@gmail.com>
2016-04-14 18:50:35 +0200

    Remove unused loop

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -2,8 +2,5 @@ class PullRequestsController < ApplicationController
   def show
     @pr = PullRequest.find(params[:id])
     @lockfile = @pr.check_lockfile!
-    @lockfile.unpatched_gems.each do |g|
-      puts g.advisory.inspect
-    end
   end
 end
