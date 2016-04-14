f14f152afdff5fe6a4b4ba31d5960cd2c69cdef1
Yannick Schutz <yannick.schutz@gmail.com>
2016-04-14 15:22:07 +0200

    Add sidekiq for activejob and default to uuid

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,9 @@ Bundler.require(*Rails.groups)
 
 module LennyTheBinoculars
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.active_job.queue_adapter = :sidekiq
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
