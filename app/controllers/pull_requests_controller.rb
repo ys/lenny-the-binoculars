ede3bd09ba01a0ef4baa84e94f8f953c334b464f
Yannick Schutz <yannick.schutz@gmail.com>
2016-04-14 18:29:13 +0200

    Fix typo

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,6 +1,6 @@
 class PullRequestsController < ApplicationController
   def show
-    @pr = PullRequests.find(params[:id])
+    @pr = PullRequest.find(params[:id])
     @lockfile = @pr.check_lockfile!
   end
 end
