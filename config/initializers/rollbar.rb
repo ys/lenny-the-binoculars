b8f760a9c84e63fcbdb3f8d37b24c479def4f1a3
Yannick Schutz <yannick.schutz@gmail.com>
2016-04-18 12:02:01 +0200

    Update app.json info about rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,6 +1,6 @@
 Rollbar.configure do |config|
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
-  config.enabled = ENV['ROLLBAR_ACCESS_TOKEN'].present? ? true : false
+  config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
+  config.enabled = ENV["ROLLBAR_ACCESS_TOKEN"].present? ? true : false
   config.use_sidekiq
-  config.environment = ENV['ROLLBAR_ENV'] || Rails.env
+  config.environment = ENV["ROLLBAR_ENV"] || Rails.env
 end
