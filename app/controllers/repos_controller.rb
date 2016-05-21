092d15f341a9e5546e247aa6da687e831bdfe2bd
Yannick Schutz <yannick.schutz@gmail.com>
2016-05-21 12:17:27 +0200

    Allow params branch

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,10 +1,14 @@
 class ReposController < ApplicationController
   def show
-    @repo = Repo.new(name_with_owner: repo_name, sha: "master")
+    @repo = Repo.new(name_with_owner: repo_name, sha: branch)
     @lockfile = @repo.check_lockfile!
   end
 
   def repo_name
    "#{params[:owner]}/#{params[:name]}"
+  end
+
+  def branch
+    params[:branch] || "master"
   end
 end
