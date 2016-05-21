4fb08cb4602d6a32837570418f712900955db442
Yannick Schutz <yannick.schutz@gmail.com>
2016-05-21 12:09:53 +0200

    Extract the repo logic

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -21,46 +21,22 @@ class PullRequest < ApplicationRecord
   end
 
   def check_lockfile!
-    raise(StandardError, "No Gemfile.lock found") unless gemfile?
-    write_gemfile_in_local_folder
-    lockfile = Lockfile.new(local_folder)
-    lockfile.scan
-    lockfile
+    repo.check_lockfile!
+  end
+
+  def gemfile_lock
+    repo.gemfile_lock
+  end
+
+  def repo
+    @repo = Repo.new(name_with_owner: repository, sha: sha)
   end
 
   def name
     "#{repository}##{number}"
   end
 
-  def gemfile?
-    gemfile_lock
-    true
-  rescue Octokit::NotFound
-    false
-  end
-
-  def gemfile_lock
-    @content ||= begin
-      encoded_content = github_file("Gemfile.lock")
-      Base64.decode64(encoded_content)
-    end
-  end
-
   private
-
-  def write_gemfile_in_local_folder
-    File.open(File.join(local_folder, "Gemfile.lock"), "w") do |f|
-      f.write gemfile_lock
-    end
-  end
-
-  def local_folder
-    @local_folder ||= Dir.tmpdir
-  end
-
-  def github_file(file)
-    github.contents(repository, ref: sha, path: file)["content"]
-  end
 
   def github
     Octokit::Client.new(access_token: ENV["GITHUB_API_TOKEN"])
