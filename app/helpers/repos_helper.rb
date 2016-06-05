f2dcaf80a3e99026be7717718bbf433d95bfbd09
Yannick Schutz <yannick.schutz@gmail.com>
2016-06-05 08:24:18 +0200

    Extract repo helpers

---
+++ app/helpers/repos_helper.rb
@@ -0,0 +1,17 @@
+module ReposHelper
+  def repo
+    Repo.new(name_with_owner: repo_name, sha: branch)
+  end
+
+  def repo_name
+    "#{params[:owner]}/#{params[:name]}"
+  end
+
+  def branch
+    params[:branch] || "master"
+  end
+
+  def repos
+    params[:repos].split(",").map(&:strip)
+  end
+end
