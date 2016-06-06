3a852df5ef8941738edfc7c874b26f5a49bd826a
Yannick Schutz <yannick.schutz@gmail.com>
2016-06-06 15:42:05 +0200

    Force return

--- app/controllers/api/repos_controller.rb
+++ app/controllers/api/repos_controller.rb
@@ -1,7 +1,7 @@
 # Repos in json
 class Api::ReposController < Api::BaseController
   def show
-    render(json: { error: "No lockfile found" }, status: :not_found) unless repo.gemfile?
+    return render(json: { error: "No lockfile found" }, status: :not_found) unless repo.gemfile?
     lockfile = repo.check_lockfile!
     render json: { name: repo.name_with_owner, sha: repo.sha }.merge(lockfile.to_h)
   end
