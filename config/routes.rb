e7079245599f6258f2ce21b20016d4225eab4a06
Yannick Schutz <yannick.schutz@gmail.com>
2016-06-08 17:14:30 +0200

    Use routes for let'sencrypt

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,14 @@ Rails.application.routes.draw do
   get "/api/:owner/:name", to: "api/repos#show"
   get "/api", to: "api/base#root"
   get "/auth/github/callback", to: "sessions#github"
+  if ENV['ACME_KEY'] && ENV['ACME_TOKEN']
+    get ".well-known/acme-challenge/#{ ENV["ACME_TOKEN"] }" => proc { [200, {}, [ ENV["ACME_KEY"] ] ] }
+  else
+    ENV.each do |var, _|
+      next unless var.start_with?("ACME_TOKEN_")
+      number = var.sub(/ACME_TOKEN_/, '')
+      get ".well-known/acme-challenge/#{ ENV["ACME_TOKEN_#{number}"] }" => proc { [200, {}, [ ENV["ACME_KEY_#{number}"] ] ] }
+    end
+  end
   root to: "root#index"
 end
